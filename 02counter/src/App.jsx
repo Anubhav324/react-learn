34187fa5bafe902582794fb7e679b970a8bef0d3
Anubhav324 <abhi1rai2234@gmail.com>
2025-12-13 13:19:21 +0530

    do

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -12,13 +12,16 @@ function App() {
     //console.log("value added",counter);
     //counter=counter+1    //value no change (react can control change in the ui of the oages based on ehat ever required)
     if(counter<20){
+    setCounter(counter+1)// use state sent all the bunch changes so writing multtiple time wint change anyhting
+    //if somebody wants to do it they should use previous state because you will only get previous state once the state is completed
+    setCounter(counter+1)
     setCounter(counter+1)}
   }
   const removeValue=() =>{
     if(counter>0){
     setCounter(counter-1)}
   }
-
+ 
   return (
     <>
       <h1>Chai aur react</h1>
